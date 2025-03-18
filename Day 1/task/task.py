d3a52344d590244f1a61bcf20d85250dbb0898fc
Jordan <jordan.sparks.business@pm.me>
2025-03-18 11:05:59 -0400

    initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,8 @@
+# Print the greeting.
+print("Welcome to the Band Name Generator.")
+# Store the city name and break to a new line.
+cityName = input("What's the name of the city you grew up in?\n")
+# Store the pet name and break to a new line.
+petName = input("What's your pet's name?\n")
+# Print the output in a combined line.
+print("Your band name could be " + cityName + " " + petName + ".")
