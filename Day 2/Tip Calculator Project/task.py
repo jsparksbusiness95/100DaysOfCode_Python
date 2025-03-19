597e1abfe2dfa3515fdbe5f732fceace829028c5
Jordan Sparks <jordan.sparks.business@pm.me>
2025-03-19 12:34:48 -0400

    initial commit

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -5,7 +5,9 @@ bill = float(input("What was the total bill?\n$"))
 tip = int(input("What percentage tip would you like to give? 10%, 12%, 15%, etc. Please do not include the % symbol.\n"))
 people = int(input("How many people to split the bill?\n"))
 
-# Portion is the bill plus the tip (divided by the total bill) then divide by the number of people.
-portion = ((bill + (bill / tip)) / people)
+# My logic
+tip_percent = round(tip / 100, 2)
+finalBill = ((round(bill * tip_percent + bill,2)) / people)
+
 # f-string to print the portion variable, rounded to the second decimal.
-print(f"Everyone needs to pay: ${round(portion, 2)}.")
+print(f"Everyone needs to pay: ${round(finalBill, 2)}.")
