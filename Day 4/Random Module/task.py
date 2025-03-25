cf54ee2abaf06c14d47ff9042d747a7d5a5fc537
Jordan <jordan.sparks.business@pm.me>
2025-03-25 10:34:36 -0400

    Uploaded Day 4 coursework.

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -3,9 +3,13 @@ import random
 # Test import of custom module
 import my_module
 
-rand_val = random.randint(1,10)
-print(f"Your random number is: {rand_val}.")
-print("\n" + my_module.quote_FotNS)
+rand_int = random.randint(1,10)
+rand_float = random.random()
+rand_float_u = random.uniform(1,10)
+print(f"Your random integer is: {rand_int}.\n")
+print(f"Your random float is: {rand_float}.\n")
+print(f"Your random uniform float is: {rand_float_u}.\n")
+print(my_module.quote_FotNS + "\n")
 
 
 # Coin toss code
