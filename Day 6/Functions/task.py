354726cd52fbbf80c0fe54a5fe116f284c5de679
Jordan <jordan.sparks.business@pm.me>
2025-04-04 11:23:00 -0400

    Day 6 coursework.

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -82,4 +82,79 @@ test()
 #     move()
 #     jump()
 
+# Reborg's World Practice Exercise 4:
+# Link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+#
+# Answer:
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# while at_goal() == False:
+#     if front_is_clear() == True:
+#         move()
+#     elif wall_in_front() == True:
+#         jump()
 
+# Reborg's World Practice Exercise 5:
+# Link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+#
+# Answer:
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     turn_left()
+#     while wall_on_right() == True:
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear() == True:
+#         move()
+#     turn_left()
+#
+# while at_goal() == False:
+#     if front_is_clear() == True:
+#         move()
+#     elif wall_in_front() == True:
+#         jump()
+
+# Reborg's World Practice Exercise 6 - Maze:
+# Link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# Answer:
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def move_north():
+#     while is_facing_north() == False:
+#         turn_left()
+#     move()
+#
+# while at_goal() == False:
+#     if right_is_clear() == True:
+#         if front_is_clear() == True:
+#             move_north()
+#         else:
+#             turn_right()
+#             move()
+#     elif wall_on_right() == True:
+#         if front_is_clear():
+#             move()
+#         else:
+#             turn_left()
